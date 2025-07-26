ac85309c68d4fa9fa0b099fbb46079faf749578c
EasyCoder <164244546+Easycoderr@users.noreply.github.com>
2025-07-26 14:40:03 +0300

    Update and fix issue switch-currency.js

--- js/switch-currency.js
+++ js/switch-currency.js
@@ -1,16 +1,24 @@
 import { getData } from "./utils/request.js";
+const currencyBtn = document.querySelectorAll(".currency-btn");
 export async function switchCurrency(page, callback) {
   const data = await getData();
-  const currencyBtn = document.querySelectorAll(".currency-btn");
-  currencyBtn.forEach((element) => {
-    element.addEventListener("click", (e) => {
-      currencyBtn.forEach((e) => e.classList.remove("active"));
-      e.target.classList.add("active");
-      if (page === "product") {
-        callback(undefined, data.value, e.target.dataset.currency);
-      } else if (page === "mycart") {
-        callback(undefined, data.value, e.target.dataset.currency);
+  if (page === "updatemycart") {
+    currencyBtn.forEach((element) => {
+      if (element.classList.contains("active")) {
+        callback(data.value, element.dataset.currency);
       }
     });
-  });
+  } else {
+    currencyBtn.forEach((element) => {
+      element.addEventListener("click", (e) => {
+        currencyBtn.forEach((e) => e.classList.remove("active"));
+        e.target.classList.add("active");
+        if (page === "product") {
+          callback(undefined, data.value, e.target.dataset.currency);
+        } else if (page === "mycart") {
+          callback(data.value, e.target.dataset.currency);
+        }
+      });
+    });
+  }
 }
