e44a41a27ba46362cd73e5955db15c81074d7219
Rauf <kurdrauf74@gmail.com>
2025-08-09 00:40:09 +0300

    Optimize currency switching by caching IQD rate in localStorage
    Previously, switching currency triggered a new request to the server every time.
    Now, the IQD exchange rate is fetched only once and stored in localStorage.
    Subsequent currency switches reuse the cached value, reducing unnecessary requests
    and improving performance.

--- js/switch-currency.js
+++ js/switch-currency.js
@@ -1,14 +1,22 @@
 import { getData } from "./utils/request.js";
 import { sortProduct } from "./sort-product.js";
 const currencyBtn = document.querySelectorAll(".currency-btn");
-async function switchCurrency(page, callback) {
+
+async function setIqdPriceInLocalStorage() {
   const data = await getData();
   localStorage.setItem("IQD", JSON.stringify(data.value));
+}
+setIqdPriceInLocalStorage();
+function getIqdPriceFromLocalStorage() {
+  return JSON.parse(localStorage.getItem("IQD"));
+}
+function switchCurrency(page, callback) {
+  const IQD = getIqdPriceFromLocalStorage();
   if (page === "updatemycart") {
     currencyBtn.forEach((element) => {
       if (element.classList.contains("active")) {
         console.log(element.dataset.currency);
-        callback(data.value);
+        callback(IQD);
       }
     });
   } else {
@@ -17,13 +25,14 @@ async function switchCurrency(page, callback) {
         currencyBtn.forEach((e) => e.classList.remove("active"));
         e.target.classList.add("active");
         if (page === "product") {
-          callback(undefined, data.value, e.target.dataset.currency);
+          callback(undefined, IQD, e.target.dataset.currency);
           sortProduct(callback);
         } else if (page === "mycart") {
-          callback(data.value, e.target.dataset.currency);
+          callback(IQD, e.target.dataset.currency);
         }
       });
     });
   }
 }
+
 export { switchCurrency };
